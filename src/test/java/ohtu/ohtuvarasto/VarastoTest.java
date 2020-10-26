a745101a4ccf19b595de05746ba612ea4a7e6204
Luukuton <mleinikka@gmail.com>
2020-10-26 17:46:50 +0200

    JUnit testing line coverage on class Varasto 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -1,23 +1,23 @@
 package ohtu.ohtuvarasto;
 
-import org.junit.*;
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class VarastoTest {
 
-    Varasto varasto;
+    Varasto varasto, varastoEmpty, varastoBalance, varastoBalanceMore, varastoBalanceEmpty, varastoBalanceNeg;
     double vertailuTarkkuus = 0.0001;
 
     @Before
     public void setUp() {
         varasto = new Varasto(10);
+        varastoEmpty = new Varasto(0);
+        varastoBalance = new Varasto(10, 1);
+        varastoBalanceMore = new Varasto(10, 11);
+        varastoBalanceEmpty = new Varasto(0, 10);
+        varastoBalanceNeg = new Varasto(10, -1);
     }
 
     @Test
@@ -56,13 +56,54 @@ public class VarastoTest {
     }
 
     @Test
-    public void ottaminenLisääTilaa() {
+    public void ottaminenLisaaTilaa() {
         varasto.lisaaVarastoon(8);
 
         varasto.otaVarastosta(2);
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaVarastoonNegatiivinen() {
+        double previousBalance = varasto.getSaldo();
+        varasto.lisaaVarastoon(-1);
+
+        // Ei muutosta saldossa.
+        assertEquals(previousBalance, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void otaVarastostaNegatiivinen() {
+        double previousBalance = varasto.getSaldo();
+        varasto.otaVarastosta(-1);
+
+        // Ei muutosta saldossa.
+        assertEquals(previousBalance, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void otaVarastostaEnemmanKuinSaldo() {
+        double previousBalance = varasto.getSaldo();
+        double withdrawnBalance = varasto.otaVarastosta(varasto.getSaldo() + 1);
+
+        // Ei muutosta saldossa.
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(withdrawnBalance, previousBalance, vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaSaldoonEnemmanKuinTilavuus() {
+        varasto.lisaaVarastoon(varasto.getTilavuus() + 1);
+
+        // Saldo enintään tilavuus.
+        assertEquals(varasto.getSaldo(), varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void merkkijonoEsitysOikein() {
+        assertEquals("saldo = 0.0, vielä tilaa 10.0", varasto.toString());
     }
 
 }
